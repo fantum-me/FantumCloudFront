13d0ad2ad2efbcd9ed7ff685a2e76d7e87b7b372
geod <geodgit@duck.com>
2024-04-03 22:07:44 +0200

    Add error checks

--- pages/docs/[id].vue
+++ pages/docs/[id].vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
 import {DocumentEditor} from "@onlyoffice/document-editor-vue";
+import type File from "~/types/api/File";
+
+const loadAllowed = ref(false)
+const error = ref()
+const config = ref()
 
 const id: string = useRoute().params.id as string
-const res = await useFetch(`/api/docs/${id}/config`)
+
+const fileRes = await useApiFetch(`/files/${id}`)
+
+if (fileRes.ok) {
+	const file: File = await fileRes.json()
+	if (isOfficeDocument(file)) {
+		if (!["doc", "ppt"].includes(file.ext)) {
+			const res = await useFetch(`/api/docs/${id}/config`)
+			if (res.status.value === "success") {
+				config.value = res.data.value?.config
+				loadAllowed.value = true
+			}
+			else error.value = "Failed to load document editor"
+		} else error.value = "The extension of this document is too old, please convert it to " + file.ext + "x"
+	} else error.value = "This document cannot be edited"
+} else error.value = "Failed to load document"
+
 </script>
 
 <template>
-	<div id="container" v-if="res.status.value === 'success'">
-		<DocumentEditor id="editor" documentServerUrl="http://localhost:7000/" :config="res.data.value.config"/>
+	<div class="w-screen h-screen flex-center">
+		<DocumentEditor v-if="loadAllowed" id="editor" documentServerUrl="http://localhost:7000/" :config="config"/>
+		<p v-else-if="error">Error: {{ error }}</p>
 	</div>
 </template>
-
-<style>
-*, ::before, ::after {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
-}
-
-#container {
-	width: 100vw;
-	height: 100vh;
-}
-</style>
