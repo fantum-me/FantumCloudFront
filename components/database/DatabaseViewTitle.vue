728a9fc434682df3cecc0aa10c7c89451b809ac6
geod <geodgit@duck.com>
2025-01-12 11:42:15 +0100

    Fix database view title can be empty.

--- components/database/DatabaseViewTitle.vue
+++ components/database/DatabaseViewTitle.vue
@@ -4,21 +4,23 @@ const database = useDatabase()
 const view = useDatabaseView()
 
 const inputRef = ref()
-const name = ref("")
+const name = ref(view.value.name)
 
-watch(() => view.value.name, newName => {
-	name.value = newName
-	resizeInput()
-}, {immediate: true})
-watch(inputRef, resizeInput)
+watch(() => view.value.name, newName => name.value = newName)
+watch(() => name.value, resizeInput)
 
 function resizeInput() {
 	if (inputRef.value)
-		inputRef.value.style.width = inputRef.value.value.length + 1 + 'ch'
+		inputRef.value.value = name.value
+	inputRef.value.style.width = inputRef.value.value.length + 1 + 'ch'
 }
 
 async function onSubmit() {
+	name.value = name.value.trim()
+	if (view.value.name === name.value) return
+	if (name.value.length === 0) name.value = capitalize(view.value.type)
 	inputRef.value.blur()
+
 	const res = await useApiFetch(`/workspaces/${workspace.value.id}/databases/${database.value.id}/views/${view.value.id}`, {
 		method: "PATCH",
 		body: JSON.stringify({name: name.value})
@@ -35,7 +37,7 @@ async function onSubmit() {
 	<div class="flex">
 		<form @submit.prevent="onSubmit">
 			<input ref="inputRef" v-model="name" class="text-xl font-semibold opacity-80 bg-transparent"
-			       @focusout="onSubmit" @input="resizeInput">
+			       @focusout="onSubmit">
 		</form>
 		<!--		<UButton icon="i-heroicons-ellipsis-horizontal" color="gray" size="sm" variant="ghost"/>-->
 	</div>
